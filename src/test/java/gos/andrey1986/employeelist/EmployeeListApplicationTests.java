bc3933d3eb293753ed19333bc9ed747b79bc8b9b
Андрей <gosudarev_a@mail.ru>
2022-05-04 21:53:16 +0300

    Initial commit

---
+++ src/test/java/gos/andrey1986/employeelist/EmployeeListApplicationTests.java
@@ -0,0 +1,13 @@
+package gos.andrey1986.employeelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
